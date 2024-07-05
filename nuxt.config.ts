7144dec103332a75653d14a97abc628f2625140d
unknown <fuleramayank@gmail.com>
2024-07-05 14:02:14 +0530

    aag

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  compatibilityDate: "2024-07-03"
+  compatibilityDate: "2024-07-03",
+  routeRules: {
+    '/': { prerender: true },
+    '/todos/**': { prerender: true }
+  },
+  hooks: {
+    'prerender:routes' ({ routes }) {
+      routes.clear() // Do not generate any routes (except the defaults)
+    }
+  },
 })
