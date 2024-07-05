b1090e51e9c17ac868204c4d96f81129c0919007
unknown <fuleramayank@gmail.com>
2024-07-05 14:06:31 +0530

    ssg

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,13 +2,13 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   compatibilityDate: "2024-07-03",
-  routeRules: {
-    '/': { prerender: true },
-    '/todos/**': { prerender: true }
-  },
-  hooks: {
-    'prerender:routes' ({ routes }) {
-      routes.clear() // Do not generate any routes (except the defaults)
-    }
-  },
+  // routeRules: {
+  //   '/': { prerender: true },
+  //   '/todos/**': { prerender: true }
+  // },
+  // hooks: {
+  //   'prerender:routes' ({ routes }) {
+  //     routes.clear() // Do not generate any routes (except the defaults)
+  //   }
+  // },
 })
